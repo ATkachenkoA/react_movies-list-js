50106736bc760d210667ac215f794c4ca4a05204
Anastasiia Tkachenko <nastya240509@gmail.com>
2026-03-23 21:29:59 +0200

    solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -26,7 +26,7 @@ export const MovieCard = ({ movie }) => (
       <div className="content">
         <p data-cy="MovieDescription">{movie.description}</p>
 
-        <a href={movie.imdbUrl} data-cy="MovieLink" key={movie.imdbId}>
+        <a href={movie.imdbUrl} data-cy="MovieLink">
           IMDB
         </a>
       </div>
